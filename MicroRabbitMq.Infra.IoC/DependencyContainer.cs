595a5de8f2c3ffec2674ae52adc3fbfc3cc403d1
Islam <eslamelnagar44@gmail.com>
2022-01-29 12:36:48 +0200

    Initial MicroRabbitMq.Banking

---
+++ MicroRabbitMq.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbitMq.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbitMq.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void Register(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
